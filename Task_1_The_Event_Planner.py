d7c1bae546064e876f28166877ac9331ef296a4d
Balbisaur <albi3479@yahoo.com>
2024-04-10 14:45:34 -0500

    first commit

---
+++ Task_1_The_Event_Planner.py
@@ -0,0 +1,11 @@
+attendees = '100'
+if attendees == '100':
+    print('large hall')
+else:
+    print('conference room')
+
+    attendees = '87'
+if attendees < '100':
+    print('large hall')
+else:
+    print('conference room')

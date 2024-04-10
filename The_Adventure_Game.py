065662a54b832a54e21703951263b88c899d64a2
Balbisaur <albi3479@yahoo.com>
2024-04-10 13:57:47 -0500

    first commit

---
+++ The_Adventure_Game.py
@@ -0,0 +1,28 @@
+place = "forest"
+
+if place == "forest":
+    print('climb a tree!')
+elif place == ('cave'):
+    print('you found hidden treasure')
+else:
+    print('go home and watch Tv :c')
+
+place = "cave"
+
+if place == "forest":
+    print('climb a tree!')
+elif place == ('cave'):
+    print('you found hidden treasure')
+else:
+    print('go home and watch Tv :c')
+   
+place = ""
+
+if place == "forest":
+    print('climb a tree!')
+elif place == ('cave'):
+    print('you found hidden treasure')
+else:
+    print('go home and watch Tv :c')
+
+#Else is just by itself, no action

def257fb9156499be37d9059b63d194866a38289
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-04-10 15:19:08 -0500

    Update Task_1_The_Event_Planner.py

--- Task_1_The_Event_Planner.py
+++ Task_1_The_Event_Planner.py
@@ -1,11 +1,33 @@
-attendees = '100'
-if attendees == '100':
-    print('large hall')
-else:
-    print('conference room')
 
-attendees = '87'
-if attendees < '100':
-    print('large hall')
-else:
-    print('conference room')
+venue = 'large hall'
+attendees = 40
+
+if venue == 'large hall':
+    if attendees <= 20:
+        print('not enough people for large hall :c')
+    elif attendees == 40:
+        print('we get the large hall!')
+    else:
+        print('no party today :c')
+
+venue = 'large hall'
+attendees = 11
+
+if venue == 'large hall':
+    if attendees <= 20:
+        print('not enough people for large hall :c')
+    elif attendees == 40:
+        print('we get the large hall!')
+    else:
+        print('no party today :c')
+
+        venue = 'large hall'
+attendees = 55
+
+if venue == 'large hall':
+    if attendees <= 20:
+        print('not enough people for large hall :c')
+    elif attendees == 40:
+        print('we get the large hall!')
+    else:
+        print('no party today :c')
